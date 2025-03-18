4fb2bd78be5ec376aaae2504eb0e62fd736b1975
Shreekar11 <shreekargade2004@gmail.com>
2025-03-18 17:52:51 +0530

    feat: create profile service added

--- src/services/profile.service.ts
+++ src/services/profile.service.ts
@@ -1,3 +1,78 @@
-export const createProfile = async () => {
+import { Profile } from "@/types";
+import { createClient } from "../../supabase/client";
 
-}
+export const createProfile = async (data: Profile) => {
+  const { first_name, last_name, about_me, experience, projects, skills } =
+    data;
+  try {
+    const supabase = await createClient();
+    const session = await supabase.auth.getUser();
+
+    const {
+      data: { user },
+    } = session;
+
+    if (!user) {
+      throw new Error("No user found");
+    }
+
+    const { data: profile, error: profileError } = await supabase
+      .from("profiles")
+      .insert({
+        first_name,
+        last_name,
+        about_me,
+      })
+      .select("id")
+      .single();
+
+    if (profileError) {
+      throw new Error(`Error inserting profile: ${profileError.message}`);
+    }
+
+    const profileId = profile.id;
+
+    const insertExperiences = experience.map((exp) => ({
+      ...exp,
+      fk_profile_id: profileId,
+    }));
+
+    const insertProjects = projects.map((proj) => ({
+      ...proj,
+      fk_profile_id: profileId,
+    }));
+
+    const insertSkills = skills.map((skill) => ({
+      ...skill,
+      fk_profile_id: profileId,
+    }));
+
+    const { error: experienceError } = await supabase
+      .from("experiences")
+      .insert(insertExperiences);
+
+    if (experienceError) {
+      throw new Error(
+        `Error inserting experiences: ${experienceError.message}`
+      );
+    }
+
+    const { error: projectError } = await supabase
+      .from("projects")
+      .insert(insertProjects);
+
+    if (projectError) {
+      throw new Error(`Error inserting projects: ${projectError.message}`);
+    }
+
+    const { error: skillError } = await supabase
+      .from("skills")
+      .insert(insertSkills);
+
+    if (skillError) {
+      throw new Error(`Error inserting skills: ${skillError.message}`);
+    }
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
